9a4246f9672388ff9d0c79bb3e8ffcad5de81177
Michael Duskanich <mduskanich@student.tgm.ac.at>
2019-10-28 23:28:49 +0100

    Imported FOVSim

---
+++ FOVsim/Assets/FOV.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOV : MonoBehaviour
+{
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+    public LayerMask obstacleMask;
+
+    public float meshRes;
+    public MeshFilter viewMeshFilter;
+    Mesh viewMesh;
+    public int edgeIterations;
+    public float edgeDstThreshold;
+
+    private void Start()
+    {
+        viewMesh = new Mesh();
+        viewMesh.name = "viewMesh";
+        viewMeshFilter.mesh = viewMesh;
+    }
+    void LateUpdate()
+    {
+        DrawFOV();
+    }
+
+    public Vector3 DirFromAngle(float angleInDeg, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDeg += -transform.eulerAngles.z;
+        }
+        return new Vector3(Mathf.Sin(angleInDeg * Mathf.Deg2Rad), Mathf.Cos(angleInDeg * Mathf.Deg2Rad), 0);
+
+    }
+    void DrawFOV()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshRes);
+        float stepAngleSize = viewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+        ViewCastInfo oldViewCast = new ViewCastInfo();
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = -transform.eulerAngles.z - viewAngle / 2 + stepAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            if (i > 0)
+            {
+                bool edgeDstThresholdExceeded = Mathf.Abs(oldViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+                if (oldViewCast.hit != newViewCast.hit || (oldViewCast.hit && (newViewCast.hit && edgeDstThresholdExceeded)))
+                {
+                    EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+                    if(edge.pointA != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointA);
+                    }
+                    if (edge.pointB != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointB);
+                    }
+                }
+            }
+
+            viewPoints.Add(newViewCast.point);
+            oldViewCast = newViewCast;
+        }
+
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+        vertices[0] = Vector3.zero;
+
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+
+        }
+
+        viewMesh.Clear();
+        viewMesh.vertices = vertices;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+    }
+
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 dir = DirFromAngle(globalAngle, true);
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), new Vector2(dir.x, dir.y), viewRadius, obstacleMask);
+        if (hit)
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
+        }
+    }
+    EdgeInfo FindEdge(ViewCastInfo min, ViewCastInfo max)
+    {
+        float minAngle = min.angle;
+        float maxAngle = max.angle;
+        Vector3 maxPoint = Vector3.zero;
+        Vector3 minPoint = Vector3.zero;
+
+        for(int i = 0; i < edgeIterations; i++)
+        {
+            float angle = (minAngle + maxAngle) / 2;
+            ViewCastInfo newViewCast = ViewCast(angle);
+            bool edgeDstThresholdExceeded = Mathf.Abs(min.dst - newViewCast.dst) > edgeDstThreshold;
+
+            if ((newViewCast.hit == min.hit) && !edgeDstThresholdExceeded)
+            {
+                minAngle = angle;
+                minPoint = newViewCast.point;
+            }
+            else
+            {
+                maxAngle = angle;
+                maxPoint = newViewCast.point;
+            }
+        }
+        return new EdgeInfo(minPoint, maxPoint);
+    }
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float dst;
+        public float angle;
+
+        public ViewCastInfo(bool _hit, Vector3 _point, float _dst, float _angle)
+        {
+            hit = _hit;
+            point = _point;
+            dst = _dst;
+            angle = _angle;
+        }
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 pointA;
+        public Vector3 pointB;
+
+        public EdgeInfo(Vector3 _pointA, Vector3 _pointB)
+        {
+            pointA = _pointA;
+            pointB = _pointB;
+        }
+    }
+}
---
+++ FOVsim/Assets/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float MovementSpeed = 3f;
+    public Rigidbody2D rb;
+    public Vector2 movemented;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movemented.x = Input.GetAxisRaw("Horizontal");
+        movemented.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movemented * MovementSpeed * Time.fixedDeltaTime);
+    }
+}
