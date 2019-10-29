961aa6b4756e85b591a9d2d152915bdf649c60fb
Michael Duskanich <mduskanich@student.tgm.ac.at>
2019-10-29 01:11:34 +0100

    FOVsim 0.2
    Added sprint, sneak and stamina.

---
+++ FOVsim/FOVsim/Assets/Editor/HotkeyYeetDelete.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.ShortcutManagement;
+using System;
+
+[InitializeOnLoadAttribute]
+public class DisableEditorShortcutsOnPlay
+{
+    private const string emptyProfile = "An Empty Profile";
+
+    // register an event handler when the class is initialized
+    static DisableEditorShortcutsOnPlay()
+    {
+        EditorApplication.playModeStateChanged += DetectPlayModeState;
+    }
+
+    private static void CreateEmptyProfile()
+    {
+        try
+        {
+            ShortcutManager.instance.CreateProfile(emptyProfile);
+        }
+        catch (Exception)
+        {
+            // Already existed, so... we good
+        }
+
+        ShortcutManager.instance.activeProfileId = emptyProfile;
+        foreach (var pid in ShortcutManager.instance.GetAvailableShortcutIds())
+            ShortcutManager.instance.RebindShortcut(pid, ShortcutBinding.empty);
+        ShortcutManager.instance.activeProfileId = ShortcutManager.defaultProfileId;
+    }
+
+    private static void DeleteEmptyProfile()
+    {
+        ShortcutManager.instance.DeleteProfile(emptyProfile);
+    }
+
+    private static void DetectPlayModeState(PlayModeStateChange state)
+    {
+        Debug.Log(state);
+        if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+            CreateEmptyProfile();
+            ShortcutManager.instance.activeProfileId = emptyProfile;
+        }
+        else
+        if (state == PlayModeStateChange.ExitingPlayMode)
+        {
+            ShortcutManager.instance.activeProfileId = ShortcutManager.defaultProfileId;
+            DeleteEmptyProfile();
+        }
+
+    }
+
+}
---
+++ FOVsim/FOVsim/Assets/movement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class movement : MonoBehaviour
+{
+    public float movingSpeed;
+    public float WalkingSpeed = 5f;
+    public float SneakSpeed = 2.5f;
+    public float SprintSpeed = 7f;
+    public float maxStamina;
+    public float stamina = 5f;
+    public Rigidbody2D rb;
+    public Vector2 movemented;
+    bool staminaRecharge = false;
+    public Slider staminaSlider;
+    public Image SliderFill;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movemented.x = Input.GetAxisRaw("Horizontal");
+        movemented.y = Input.GetAxisRaw("Vertical");
+
+        StaminaManagement();
+
+        if (Input.GetButton("Sneak"))
+        {
+            movingSpeed = SneakSpeed;
+        }else if (Input.GetButton("Sprint") && stamina > 0 && !staminaRecharge && movemented != Vector2.zero)
+        {
+            movingSpeed = SprintSpeed;
+            stamina -= Time.fixedDeltaTime;
+            if(stamina <= 0)
+            {
+                staminaRecharge = true;
+                movingSpeed = 5;
+            }
+        }
+        else
+        {
+            movingSpeed = WalkingSpeed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movemented * movingSpeed * Time.fixedDeltaTime);
+    }
+
+
+    public void StaminaManagement()
+    {
+        if (stamina <= maxStamina && !Input.GetButton("Sprint") || staminaRecharge || movemented == Vector2.zero)
+        {
+            stamina += Time.fixedDeltaTime;
+        }
+        if (stamina > maxStamina)
+        {
+            stamina = maxStamina;
+        }
+        if (stamina >= maxStamina)
+        {
+            staminaRecharge = false;
+        }
+        staminaSlider.value = stamina / maxStamina;
+        if (staminaRecharge)
+        {
+            SliderFill.color = new Color(1f, 0f, 0f);
+        }
+        else
+        {
+            SliderFill.color = new Color(1 - stamina / maxStamina, stamina / maxStamina, 0f);
+        }
+    }
+}
